c856e4ded52c0bf6d02f6bf7b9e569d03ac45b07
Wong <cwo11@mls-sc32154.childrens.sea.kids>
2024-02-01 11:57:35 -0800

    added extract_summit untilities

--- R/macs2_utilities.R
+++ R/macs2_utilities.R
@@ -12,14 +12,6 @@
 #'                            package='peaklerrr')
 #' gr <- read_macs2_narrow(narrow_file)
 #' gr
-
-# broadPeaks
-#' broad_file <- system.file('extdata', 
-#'                           'chr2_Rep1_H1_H3K27me3_peaks.broadPeak',
-#'                            package='peaklerrr')
-#' gr <- read_macs2_broad(broad_file)
-#' gr
-#' 
 #' @export
 read_macs2_narrow <- function(file, ...) {
   # sanity check
@@ -36,6 +28,22 @@ read_macs2_narrow <- function(file, ...) {
                       extraCols = extraCols_narrowPeak, ...) 
 }
 
+#' Import MACS2 broadPeak and broadPeak BED file and format
+#' ranges to a object
+#' @file: a path to a file or a connection
+#' @...: parameters pass to rtracklayer::import.bed()
+#' 
+#' @return a GRanges object
+#' @rdname read_macs2
+#' @importFrom rtracklayer import
+#' @example
+#' broadPeaks
+#' broad_file <- system.file('extdata', 
+#'                           'chr2_Rep1_H1_H3K27me3_peaks.broadPeak',
+#'                            package='peaklerrr')
+#' gr <- read_macs2_broad(broad_file)
+#' gr
+#' 
 #' @export
 read_macs2_broad <- function(file, ...) {
   # sanity check
@@ -49,4 +57,36 @@ read_macs2_broad <- function(file, ...) {
   
   rtracklayer::import(file, format = 'BED',
                       extraCols = extraCols_broadPeak, ...) 
+}
+
+#' Exact MACS2 narrow peak summit and convert to a GRanges object
+#' @gr: A GRanges of MACS2 narrow peak, and it should include the 'max.signal.region' in the metadata columns
+#' @summit_wid: NULL (default) or positive integer indicating the width of summit. Default to the width of 'max.signal.region'
+#' 
+#' @return a GRanges object
+#' @rdname extract_summit
+#' @importFrom plyranges as_granges
+#' @examples
+#' narrow_file <- system.file('extdata', 
+#'                            'chr2_Rep1_H1_CTCF_peaks.narrowPeak', 
+#'                            package='peaklerrr')
+#' gr <- read_macs2_narrow(narrow_file)
+#' summit <- extrac_summit_macs2(gr, summit_wid=1L)
+#' summit
+#' @export
+extract_summit_macs2 <- function(gr, summit_wid = NULL) {
+  
+  if (is.null(summit_wid)) summit_wid <- 1L
+  if (!is.integer(summit_wid)) summit_wid <- as.integer(summit_wid) 
+  if (summit_wid <= 0L)
+    stop('Summit width must be greater than 0L.')
+  
+  
+  summit <- data.frame(start=start(gr) + gr$peak,
+                       width = summit_wid,
+                       seqnames = seqnames(gr),
+                       strand = '*')
+  summit <- plyranges(as_granges())
+  mcols(summit) <- mcols(gr)[, names(mcols(gr)) != 'peak']
+  summit
 }
--- R/seacr_utilities.R
+++ R/seacr_utilities.R
@@ -11,7 +11,6 @@
 #' gr <- read_seacr(seacr_file)
 #' gr
 #'
-
 #' @export
 read_seacr <- function(file) {
   # sanity check
@@ -32,17 +31,43 @@ read_seacr <- function(file) {
   
 }
 
-
-
-summit_from_seacr <- function(seacr_gr, summit_wid = NULL) {
-  # convert to seacr max.signal.region column to GRanges
+#' Exact SEACR peak summit and convert to a GRanges object
+#' @gr: A GRanges representing a peaks yield by SEACR, and it should include the 'max.signal.region' in the metadata columns
+#' @summit_wid: NULL (default) or positive integer indicating the width of summit. Default to the width of 'max.signal.region'
+#' 
+#' @return a GRanges object with columns name and score (max.signal)
+#' @rdname extract_summit
+#' @examples
+#' seacr_file <- system.file('extdata',
+#'                           'chr2_Rep1_H1_CTCF.stringent.bed',
+#'                            package='peaklerrr')
+#' gr <- read_seacr(seacr_file)
+#' summit <- extract_summit_seacr(gr, summit_wid=1L)
+#'
+#' @export
+extract_summit_seacr <- function(gr, summit_wid = NULL) {
+  # convert to SEACR peak max.signal.region column to GRanges
+  
+  
+  if (!is.null(summit_wid) & !is.integer(summit_wid)) 
+    summit_wid <- as.integer(summit_wid) 
+  if (!is.null(summit_wid) & summit_wid <= 0L) {
+    message('Summit width must be greater than 0L. Default to NULL')
+    summit_wid <- NULL
+  }
+  
+  if ('max.signal.region' %in% names(mcols(gr)))
+    stop('The metadata max.signal.region column does not exist.')
+  
   summit <- GRanges(gr$max.signal.region) %>%
     plyranges::mutate(name = paste0('peakname_', 1:length(gr)),
                       score = gr$max.signal, 
                       itemRgb ='#0000FF')
+  
   if (!is.null(summit_wid) & is.integer(summit_wid)) {
     summit <- 
       plyranges::mutate(anchor_center(summit), width = summit_wid)
   }
+  
   return(summit)
 }
