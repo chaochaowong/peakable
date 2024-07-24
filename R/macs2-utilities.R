620af2406009857f9d39c483425ecd8bc6901e62
Chao-Jen Wong <chao-jen.wong@seattlechildrens.org>
2024-07-24 12:43:12 -0700

    fixed bugs in read_macs2_broad

--- R/macs2-utilities.R
+++ R/macs2-utilities.R
@@ -2,46 +2,46 @@
 #' ranges to a object
 #' @file: a path to a file or a connection
 #' @...: parameters pass to rtracklayer::import.bed()
-#' 
+#'
 #' @return a GRanges object
 #' @rdname read_macs2
 #' @importFrom rtracklayer import
 #' @importFrom GenomeInfoDb dropSeqlevels keepStandardChromosomes seqlevelsStyle
 #' @examples
-#' narrow_file <- system.file('extdata', 
-#'                            'chr2_Rep1_H1_CTCF_peaks.narrowPeak', 
+#' narrow_file <- system.file('extdata',
+#'                            'chr2_Rep1_H1_CTCF_peaks.narrowPeak',
 #'                            package='peaklerrr')
 #' gr <- read_macs2_narrow(narrow_file)
 #' gr
 #' @export
-read_macs2_narrow <- function(file, drop_chrM = FALSE, 
+read_macs2_narrow <- function(file, drop_chrM = FALSE,
                               keep_standard_chrom = FALSE,
                               species = NULL, ...) {
   # sanity check
   stopifnot(length(file) == 1)
-  stopifnot(file.exists(file)) 
-  
+  stopifnot(file.exists(file))
+
   # define extra columns
-  extraCols_narrowPeak <- c(signalValue = "numeric", 
+  extraCols_narrowPeak <- c(signalValue = "numeric",
                             pValue = "numeric",
-                            qValue = "numeric", 
+                            qValue = "numeric",
                             peak = "integer")
-  
-  gr <- rtracklayer::import(file, format = 'BED', 
-                            extraCols = extraCols_narrowPeak, ...) 
-  
+
+  gr <- rtracklayer::import(file, format = 'BED',
+                            extraCols = extraCols_narrowPeak, ...)
+
   if (keep_standard_chrom & length(gr) > 0)
     gr <- GenomeInfoDb::keepStandardChromosomes(gr, pruning.mode='coarse',
                                                 species = species)
-  
+
   if (drop_chrM & length(gr) > 0) {
     if (GenomeInfoDb::seqlevelsStyle(gr) == 'UCSC') value = 'chrM'
     if (GenomeInfoDb::seqlevelsStyle(gr) == 'NCBI') value = 'M'
-    if (value %in% seqlevels(gr)) 
+    if (value %in% seqlevels(gr))
       gr <- GenomeInfoDb::dropSeqlevels(gr, value=value,
                                         pruning.mode='coarse')
   }
-  
+
   return(gr)
 }
 
@@ -49,80 +49,85 @@ read_macs2_narrow <- function(file, drop_chrM = FALSE,
 #' ranges to a object
 #' @file: a path to a file or a connection
 #' @...: parameters pass to rtracklayer::import.bed()
-#' 
+#'
 #' @return a GRanges object
 #' @rdname read_macs2
 #' @importFrom rtracklayer import
 #' @importFrom GenomeInfoDb dropSeqlevels keepStandardChromosomes seqlevelsStyle
 #' @example
 #' broadPeaks
-#' broad_file <- system.file('extdata', 
+#' broad_file <- system.file('extdata',
 #'                           'chr2_Rep1_H1_H3K27me3_peaks.broadPeak',
 #'                            package='peaklerrr')
 #' gr <- read_macs2_broad(broad_file)
 #' gr
-#' 
+#'
 #' @export
-read_macs2_broad <- function(file, drop_chrM = FALSE, 
+read_macs2_broad <- function(file, drop_chrM = FALSE,
                              keep_standard_chrom = FALSE,
                              species = NULL, ...) {
   # sanity check
   stopifnot(length(file) == 1)
-  stopifnot(file.exists(file)) 
-  
+  stopifnot(file.exists(file))
+
   # define extra columns
-  extraCols_broadPeak <- c(signalValue = "numeric", 
+  extraCols_broadPeak <- c(signalValue = "numeric",
                            pValue = "numeric",
                            qValue = "numeric")
-  
+
+  # what's the species? must assign species or genome
   gr <- rtracklayer::import(file, format = 'BED',
-                      extraCols = extraCols_broadPeak, ...) 
-  
-  if (keep_standard_chrom)
+                      extraCols = extraCols_broadPeak, ...)
+
+  # verify the sequence levels match the species
+
+  if (keep_standard_chrom & length(gr) > 0)
     gr <- GenomeInfoDb::keepStandardChromosomes(gr, pruning.mode='coarse',
                                                 species = species)
-  
-  if (drop_chrM) {
+
+  if (drop_chrM & length(gr) > 0) {
+
     if (GenomeInfoDb::seqlevelsStyle(gr) == 'UCSC') value = 'chrM'
     if (GenomeInfoDb::seqlevelsStyle(gr) == 'NCBI') value = 'M'
-    if (value %in% seqlevels(gr)) 
+    if (value %in% seqlevels(gr))
       gr <- GenomeInfoDb::dropSeqlevels(gr, value=value,
                                         pruning.mode='coarse')
   }
+
   return(gr)
 }
 
 #' Exact MACS2 narrow peak summit and convert to a GRanges object
 #' @gr: A GRanges of MACS2 narrow peak, and it should include the 'max.signal.region' in the metadata columns
 #' @summit_wid: NULL (default) or positive integer indicating the width of summit. Default to the width of 'max.signal.region'
-#' 
+#'
 #' @return a GRanges object
 #' @rdname extract_summit
 #' @importFrom plyranges as_granges
 #' @examples
-#' narrow_file <- system.file('extdata', 
-#'                            'chr2_Rep1_H1_CTCF_peaks.narrowPeak', 
+#' narrow_file <- system.file('extdata',
+#'                            'chr2_Rep1_H1_CTCF_peaks.narrowPeak',
 #'                            package='peaklerrr')
 #' gr <- read_macs2_narrow(narrow_file)
 #' summit <- extrac_summit_macs2(gr, summit_wid=1L)
 #' summit
 #' @export
 extract_summit_macs2 <- function(gr, summit_wid = NULL) {
-  
+
   # check mcols are MACS2-specific
   if (!'peak' %in% names(mcols(gr)))
     stop('gr must contain MACS2 narrowPeak specific column "peak".')
-  
+
   if (is.null(summit_wid)) summit_wid <- 1L
-  if (!is.integer(summit_wid)) summit_wid <- as.integer(summit_wid) 
+  if (!is.integer(summit_wid)) summit_wid <- as.integer(summit_wid)
   if (summit_wid <= 0L)
     stop('Summit width must be greater than 0L.')
-  
+
   # assuming the peak is not strand-sensitive
   summit <- gr %>%
-    plyranges::mutate(start = start + peak, width=1L) 
+    plyranges::mutate(start = start + peak, width=1L)
   summit <- plyranges::mutate(anchor_center(summit), width=summit_wid)
   mcols(summit) <- mcols(summit)[, names(mcols(summit)) != 'peak']
-  
+
   return(summit)
 }
--- R/peakle_flow.R
+++ R/peakle_flow.R
@@ -1,38 +1,38 @@
 #' peakle_flow.R
-#' @example 
+#' @example
 #' /donotrun{
 #' dir_header <- '///Volumes' # via MacBook
 #' result_dir <- file.path(dir_header, 'Active',
 #'                         'lawlor_e/Shireen/CnT_Results',
 #'                         'A673_anthracyclines_240130')
-#' sample_df <- read_csv(file.path(result_dir, 'data', 'nf-sample-sheet.csv'))                         
-#' 
+#' sample_df <- read_csv(file.path(result_dir, 'data', 'nf-sample-sheet.csv'))
+#'
 #' peak_bed_dir <- file.path(result_dir, 'peaks_calls', 'seacr_callpeak')
 #' peak_bed_pattern <- '\\_threshold0.01_non.stringent.bed$'
-#' seacr <- 
-#'   peakable:::peakle_flow(sample_df, 
+#' seacr <-
+#'   peakable:::peakle_flow(sample_df,
 #'                           result_dir, result_dir,
 #'                           peak_caller = 'SEACR-thres1p',
 #'                           peak_bed_dir = peak_bed_dir,
 #'                           peak_bed_pattern = peak_bed_pattern)
 #'
 #'  seacr_hit_mat <- peakable::consolidated_peak_hits(seacr$grl)
-#' seacr_hit_pca <- 
-#'   peaklable:::.getPCA(seacr_hit_mat, 
+#' seacr_hit_pca <-
+#'   peaklable:::.getPCA(seacr_hit_mat,
 #'                       sample_info=seacr$df, n_pcs=2)
-#' ggplot(seacr_hit_pca, aes(x=PC1, y=PC2, 
+#' ggplot(seacr_hit_pca, aes(x=PC1, y=PC2,
 #'                          color=antibody, shape=treatment)) +
 #'   geom_point() + theme_minimal() +
 #'   labs(title='PCA: SEACR peak-hits matrix')
-#'}   
+#'}
 .set_bam_params <- function(result_dir, bam_pattern) {
   #' ignore bam_dir and bam_pattern
   bam_dir <- file.path(result_dir, 'samtools_sort')
 
   if (is.null(bam_pattern)) {
     bam_pattern <- '\\.markedDup.filter.sort.bam$'
-  } 
-  
+  }
+
   #' sanity checK does the bam file exists?
   return(list(bam_dir = bam_dir, bam_pattern = bam_pattern))
 }
@@ -55,14 +55,14 @@
 
 .samtools_stats <- function(path, pattern) {
   if (!file.exists(path)) stop('samtools_stats path does not exist.')
-  
+
   # get duplication rate
-  
+
   stats_df <- .get_list_files_and_sample_id(path, pattern)
-  
+
   # parse the log
   stats_log <- purrr::map_dfr(stats_df$file, function(x) {
-    ln <- readr::read_delim(x, delim="\t", skip=16, 
+    ln <- readr::read_delim(x, delim="\t", skip=16,
                             n_max=3, col_names=FALSE)[, -1]
     data.frame(sample_id = str_replace(basename(x), '.markedDup.stats', ''),
                read_paired = ln[2, 'X3', drop=TRUE],
@@ -72,22 +72,22 @@
 
 peakle_flow <- function(sample_df, # must be from nf_sample_sheet
                         result_dir,
-                        peak_caller, 
-                        peak_bed_dir, 
+                        peak_caller,
+                        peak_bed_dir,
                         peak_bed_pattern,
                         bam_pattern = NULL) {
   library(tidyverse)
   require(purrr)
   require(BiocParallel)
-  
+
   #' setting parameters:
   #' if result_dir is given, then use default bam directory
   if (!file.exists(result_dir))
     stop(result_dir, ' does not exist.')
-  
+
   if (!file.exists(peak_bed_dir))
     stop(peak_bed_dir, ' does not exist.')
-  
+
   # define parameters and patterns
   bam_params <- .set_bam_params(result_dir, bam_pattern)
   bam_dir <- bam_params$bam_dir
@@ -98,28 +98,28 @@ peakle_flow <- function(sample_df, # must be from nf_sample_sheet
 
   # check the input sample_df is valid
   # check if stats_dir exists, if not, skip samtools stats
-  
+
   # 1.a) bam files; sample_id is the basename with bam_patter replaced
   bam_df <- .get_list_files_and_sample_id(bam_dir, bam_pattern) %>%
     dplyr::rename(bam_file = 'file')
-  
+
   # check if stats_dir exists
   if (file.exists(stats_dir) ) {
      stats_df <- .samtools_stats(stats_dir, stats_pattern)
   } else {
     stats_df <- NULL
   }
-     
-  
+
+
   # 2) append to sample_df; remove irrelavent columns
   sample_df <- sample_df %>%
-    dplyr::left_join(bam_df, by='sample_id') 
-  
+    dplyr::left_join(bam_df, by='sample_id')
+
   if (!is.null(stats_df)) {
     sample_df <- sample_df %>%
       dplyr::left_join(stats_df, by='sample_id')
   }
-  
+
   # 3) get peak bed files
   peak_df <- data.frame(
     bed_file = list.files(peak_bed_dir, pattern=peak_bed_pattern,
@@ -128,44 +128,46 @@ peakle_flow <- function(sample_df, # must be from nf_sample_sheet
                                           peak_bed_pattern, ''),
                   peak_caller = peak_caller) %>%
     # if relaxed or stringent compared with IgG, then tidy sample_id
-    dplyr::mutate(sample_id =  str_split(peakcall_id, '_vs_', 
+    dplyr::mutate(sample_id =  str_split(peakcall_id, '_vs_',
                                          simplify=TRUE)[, 1]) %>%
     dplyr::right_join(sample_df, by='sample_id') %>%
     dplyr::filter(!is.na(bed_file)) # some IgG files do not have bed files
-  
-  # additional column for the IgG file
-  if (str_detect(peak_caller, 'relaxed|stringent')) {
-    peak_df <- peak_df %>%
-      dplyr::mutate(IgG = str_split(peakcall_id, '_vs_', 
-                                    simplify=TRUE)[, 2])
+
+  # additional column for the IgG file: if caller is SEACR
+  if (str_detect(peak_caller, 'SEACR')) { # not detect threshold
+    if (str_detect(peak_caller, 'relaxed|stringent')) {
+       peak_df <- peak_df %>%
+        dplyr::mutate(IgG = str_split(peakcall_id, '_vs_',
+                                      simplify=TRUE)[, 2])
+    }
   }
-  
+
   # tidyup and remove peakcall_id
   peak_df <- peak_df %>% dplyr::select(-peakcall_id)
-  
+
   # 4) sanity check
   if (nrow(peak_df) < 1)
     stop('Cannot not fine the peak bed files for these samples.')
   # 4) define the peak ranges
 
-  if (str_detect(peak_caller, 'SEACR')) 
+  if (str_detect(peak_caller, 'SEACR'))
     peak_call_func <- peakable::read_seacr
   if (str_detect(peak_caller, 'narrow'))
     peak_call_func <- peakable::read_macs2_narrow
   if (str_detect(peak_caller, 'broad'))
     peak_call_func <- peakable::read_macs2_broad
-  
+
   message('Peak callers: ', peak_caller)
   message('Get peak ranges ...')
-  
-  peak_grl <- bplapply(peak_df$bed_file, 
+
+  peak_grl <- bplapply(peak_df$bed_file,
                        peak_call_func,
                        drop_chrM = TRUE,
                        keep_standard_chrom = TRUE,
                        species = 'Homo_sapiens')
   names(peak_grl) <- peak_df$sample_id
-  
+
   peak_df$number_of_peaks <- sapply(peak_grl, length)
-  
+
   return(list(df = peak_df, grl = peak_grl))
 }
